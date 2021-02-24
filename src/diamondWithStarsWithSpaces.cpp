5b51996a224d1d4f9aa9a09b952a95b1ab8c4035
Gs Rawat <gsrawat0512@gmail.com>
2021-02-24 10:05:36 +0530

    add diamond stars pattern

--- src/diamondWithStarsWithSpaces.cpp
+++ src/diamondWithStarsWithSpaces.cpp
@@ -9,7 +9,16 @@ int main() {
             cout << " ";
         }
         for(int k = 0; k <= i; k++) {
-            cout << "*";
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    for(int i = n-2; i >= 0; i--) {
+        for(int j = 0; j < n-i-1; j++) {
+            cout << " ";
+        }
+        for(int k = 0; k <= i; k++) {
+            cout << "* ";
         }
         cout << endl;
     }
